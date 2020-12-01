72b4a650108d636bce68301c529aa333f7072b8b
shajoffcl <shaj.native@gmail.com>
2020-12-01 23:16:35 +0530

    Updates

---
+++ precache-manifest.c81d5ec05564c6b8d732eea76f0fe0f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abf089663bb0aed94b4579cb4b644df",
+    "url": "/github-host-app/index.html"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/github-host-app/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-host-app/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720a484d117e768fe360",
+    "url": "/github-host-app/static/js/main.2d394cfd.chunk.js"
+  },
+  {
+    "revision": "bd51e1afdf985e921f1d",
+    "url": "/github-host-app/static/js/runtime-main.51e88295.js"
+  }
+]);
